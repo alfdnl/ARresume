31259bdac5d64a6cc1e31ae83e3d4d7a81385075
alfdnl <alfdnl080@siswa.um.edu.my>
2019-04-17 18:54:55 +0800

    link updated and added two 3d model

--- events.js
+++ events.js
@@ -2,11 +2,19 @@ AFRAME.registerComponent('markerhandler', {
 
     init: function() {
         const animatedMarker = document.querySelector("#animated-marker");
-        const aEntity = document.querySelector("#animated-model");
+        // const aEntity = document.querySelector("#animated-model");
 
         // every click, we make our model grow in size :)
-        animatedMarker.addEventListener('click', function(ev, target){         
-            console.log("test");
-            window.location.href = "http://stackoverflow.com";
+        animatedMarker.addEventListener('click', function(ev, target){
+        	if (ev.target.id === "animated-model") {
+            console.log("sulses");
+            window.location.href = "https://www.instagram.com/alf.dnl/";
+        	}
+			if (ev.target.id === "animated-model2") {
+            console.log("sulses2");
+            window.location.href = "https://twitter.com/alipnoi";
+        	}
+            
         });
-}});
+	}
+});
